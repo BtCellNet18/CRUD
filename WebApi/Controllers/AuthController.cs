df75ec89bfab248eda79b83a6f38e4b3bedb98ed
Brendan McTeggart <brendanmcteggart@gmail.com>
2020-02-13 21:28:19 +0000

    Remove HTTP 500 from AuthController Login method

--- WebApi/Controllers/AuthController.cs
+++ WebApi/Controllers/AuthController.cs
@@ -32,12 +32,10 @@ namespace WebApi.Controllers
 		/// </summary> 
 		/// <response code="200">OK</response> 
 		/// <response code="400">Bad Request</response>  
-		/// <response code="500">Internal Server Error</response>  
 		[AllowAnonymous]
 		[HttpPost]
 		[ProducesResponseType(200, Type = typeof(TokenResponse))]
 		[ProducesResponseType(400, Type = typeof(void))]
-		[ProducesResponseType(500, Type = typeof(void))]
 		public async Task<ActionResult> Login([FromBody]LoginUserRequest request)
 		{
 			var query = new GetTokenQuery(request.Username, request.Password);
